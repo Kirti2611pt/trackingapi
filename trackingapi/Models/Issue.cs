261b08a7edaba6972b220b0ec37e0842c3d00fee
Kirti Prakash Tishu <talktokirtiprakash@gmail.com>
2022-11-09 12:25:35 +0530

    Add project files.

---
+++ trackingapi/Models/Issue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trackingapi.Models
+{
+    public class Issue
+    {
+        [Required]
+        public int Id { get; set; }
+
+        private string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+    public enum Priority
+    {
+        Low,Medium, High
+    }
+    public enum IssueType
+    {
+        Feature,Bug,Documentation
+    }
+}
